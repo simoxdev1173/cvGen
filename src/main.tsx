b0ccba345e100f6ba1c4d825ee7411fc63d386ce
Simo_Elattar <simotravail1173@gmail.com>
2025-04-28 22:32:37 +0100

    added hashRouting

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/cvGen">
+    <HashRouter basename="/cvGen">
       <App />
-    </BrowserRouter>
+    </HashRouter>
 
   </StrictMode>,
 )
